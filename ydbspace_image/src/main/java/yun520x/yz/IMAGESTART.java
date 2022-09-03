b51909aa4bb832cbd6645ce83bc8ba9e61655311
1510557673@qq.com <Aa1997925>
2022-09-03 16:14:19 +0800

    Initial commit

---
+++ ydbspace_image/src/main/java/yun520x/yz/IMAGESTART.java
@@ -0,0 +1,11 @@
+package yun520x.yz;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class IMAGESTART {
+    public static void main(String[] args) {
+        SpringApplication.run(IMAGESTART.class, args);
+    }
+}
