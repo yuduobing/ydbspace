bb7684f281672a184f4cb9dae47a326f76733ca7
1510557673@qq.com <Aa1997925>
2023-01-04 16:52:24 +0800

    改名

---
+++ User/src/main/java/yun520/xyz/config/AuthorizationServerConfig.java
@@ -0,0 +1,50 @@
+package yun520.xyz.config;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Qualifier;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.security.authentication.AuthenticationManager;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.crypto.password.PasswordEncoder;
+import org.springframework.security.oauth2.config.annotation.configurers.ClientDetailsServiceConfigurer;
+import org.springframework.security.oauth2.config.annotation.web.configuration.AuthorizationServerConfigurerAdapter;
+import org.springframework.security.oauth2.config.annotation.web.configuration.EnableAuthorizationServer;
+import org.springframework.security.oauth2.config.annotation.web.configuration.EnableResourceServer;
+import org.springframework.security.oauth2.config.annotation.web.configuration.ResourceServerConfigurerAdapter;
+import org.springframework.security.oauth2.config.annotation.web.configurers.AuthorizationServerEndpointsConfigurer;
+import org.springframework.security.oauth2.provider.token.TokenStore;
+import yun520.xyz.Service.UserService;
+
+/**
+ * 认证服务器配置
+ * Created by macro on 2019/9/30.
+ */
+@Configuration
+@EnableAuthorizationServer
+public class AuthorizationServerConfig extends AuthorizationServerConfigurerAdapter {
+
+    @Autowired
+    private PasswordEncoder passwordEncoder;
+
+    @Autowired
+    private AuthenticationManager authenticationManager;
+
+    @Autowired
+    private UserService userService;
+
+    @Autowired
+    @Qualifier("redisTokenStore")
+    private TokenStore tokenStore;
+
+    /**
+     * 使用密码模式需要配置
+     */
+    @Override
+    public void configure(AuthorizationServerEndpointsConfigurer endpoints) {
+        endpoints.authenticationManager(authenticationManager)
+                .userDetailsService(userService)
+                .tokenStore(tokenStore);//配置令牌存储策略
+    }
+
+    //省略代码...
+}
--- fileRedis/src/main/java/yun520/xyz/FileRedisStart.java
+++ fileRedis/src/main/java/yun520/xyz/FileRedisStart.java
@@ -5,10 +5,10 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
-public class RedisStart {
+public class FileRedisStart {
     public static void main(String[] args) {
 
-        SpringApplication.run(RedisStart.class, args);
+        SpringApplication.run(FileRedisStart.class, args);
     }
 
 }
