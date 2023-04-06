ec3f328e3cec6eb1c363257aff8ac590d4b448d2
1510557673@qq.com <Aa1997925>
2023-04-06 12:02:11 +0800

    修改了inputstream，现在返回文件中心返回inputstream

---
+++ fileMq/src/main/java/yun520/xyz/FileMqStart.java
@@ -0,0 +1,13 @@
+package yun520.xyz;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class FileMqStart {
+    public static void main(String[] args) {
+
+        SpringApplication.run(FileMqStart.class, args);
+    }
+
+}
