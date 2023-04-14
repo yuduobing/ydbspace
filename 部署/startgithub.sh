5ac2831b4f787c2db468ec445a57a1397cb4a151
1510557673@qq.com <Aa1997925>
2023-04-14 09:59:59 +0800

    修改文件夹3

--- 部署/startgithub.sh
+++ 部署/startgithub.sh
@@ -1,3 +1,12 @@
+#shell学习
+#在shell脚本中，使用${}语法可以帮助您更好地引用变量。${}可以明确地将变量名与其他字符分离，从而避免错误地解释变量值。
+#
+#例如，如果您有一个名为VAR的变量，您可以使用以下任一语法来引用它：
+#
+#echo $VAR
+#或者
+#
+#echo ${VAR}
 ##todo1 设置jar包位置
 DEPLOY_PATH=/docker/部署/ydbspace_github
 mkdir -p $DEPLOY_PATH 
@@ -9,39 +18,27 @@ jarname[2]=filemq-exec.jar
 jarname[3]=User.jar
 
 cd $DEPLOY_PATH
-echo "循环停止${DEPLOY_PATH}jar包开始"
+echo "${DEPLOY_PATH}jar包开始执行啦。噜啦啦噜啦啦"
 for ((i = 0; i < ${#jarname[@]}; i++)); do
   #${#jarname[@]}获取数组长度用于循环
   echo ${jarname[i]}
 
   #    停止jar包
   APP_NAME=${jarname[i]}
-  echo '开始执行------------' $APP_NAME
+  echo '$APP_NAME开始执行------------'
   tpid=$(ps -ef | grep $APP_NAME | grep -v grep | grep -v kill | awk '{print $2}')
   if [ ${tpid} ]; then
-    echo 'Stop Process...' $APP_NAME
+    echo '停止进程' $APP_NAME
     kill -15 $tpid
   fi
   sleep 5
   tpid=$(ps -ef | grep $APP_NAME | grep -v grep | grep -v kill | awk '{print $2}')
   if [ ${tpid} ]; then
-    echo 'Kill Process!'
+    echo '杀死进程'
     kill -9 $tpid
   fi
-  echo "启动 jar包"
+  echo "启动jar包：  java -jar $APP_NAME --spring.profiles.active=prod >$APP_NAME.log & "
   #   指定生产环境包
   nohup java -jar $APP_NAME --spring.profiles.active=prod >$APP_NAME.log &
-  echo "---------------"
+  echo '$APP_NAME结束执行------------'
 done
-
-
-
-#shell学习
-#在shell脚本中，使用${}语法可以帮助您更好地引用变量。${}可以明确地将变量名与其他字符分离，从而避免错误地解释变量值。
-#
-#例如，如果您有一个名为VAR的变量，您可以使用以下任一语法来引用它：
-#
-#echo $VAR
-#或者
-#
-#echo ${VAR}
