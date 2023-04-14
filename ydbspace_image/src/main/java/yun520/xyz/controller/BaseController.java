20145c2406f15675e7cffd36ee9a8dc7d61ca231
1510557673@qq.com <Aa1997925>
2023-04-14 10:31:03 +0800

    修改文件夹3

--- ydbspace_image/src/main/java/yun520/xyz/controller/BaseController.java
+++ ydbspace_image/src/main/java/yun520/xyz/controller/BaseController.java
@@ -11,28 +11,31 @@ import yun520.xyz.service.LoginService;
 public class BaseController {
     @Autowired
     private LoginService loginService;
-    public  Long getUserid(){
+
+    public Long getUserid() {
         JSONObject user = getUser();
         long userid = Long.valueOf((String) user.get("userid"));
-        Assert.notNull(userid,"获得userid为空");
+        Assert.notNull(userid, "获得userid为空");
         return userid;
 
     }
-    //通过token去user服务调用
-    public JSONObject getUser(){
-        Object details = SecurityContextHolder.getContext().getAuthentication().getDetails();
-        Assert.notNull(details,"获得用户details为空");
-        String authention="bearer "+new JSONObject(details).get("tokenValue");
-        Object userInformation = loginService.getUserInformation(authention);
-        Assert.notNull(userInformation,"获得用户信息为空");
-        return    new JSONObject(userInformation);
-    }
-    public  Long getUserid(String authention){
 
-        Assert.notNull(authention,"获得用户authention为空");
+    //通过token去user服务调用
+    public JSONObject getUser() {
+        Object details = SecurityContextHolder.getContext().getAuthentication().getDetails();
+        Assert.notNull(details, "获得用户details为空");
+        String authention = "bearer " + new JSONObject(details).get("tokenValue");
         Object userInformation = loginService.getUserInformation(authention);
-        long userid = Long.valueOf((String) new JSONObject( userInformation).get("userid"));
-        Assert.notNull(userid,"获得userid为空");
+        Assert.notNull(userInformation, "获得用户信息为空");
+        return new JSONObject(userInformation);
+    }
+
+    public Long getUserid(String authention) {
+
+        Assert.notNull(authention, "获得用户authention为空");
+        Object userInformation = loginService.getUserInformation(authention);
+        long userid = Long.valueOf((String) new JSONObject(userInformation).get("userid"));
+        Assert.notNull(userid, "获得userid为空");
         return userid;
 
     }
