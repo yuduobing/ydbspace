319befe295126487c7811b452fd7eb4de63d43f4
1510557673@qq.com <Aa1997925>
2023-06-09 17:28:38 +0800

    阿里云sdk修复2

--- ydbspace_image/src/main/java/yun520/xyz/ImageApplication.java
+++ ydbspace_image/src/main/java/yun520/xyz/ImageApplication.java
@@ -3,18 +3,13 @@ package yun520.xyz;
 import org.mybatis.spring.annotation.MapperScan;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
+
 import org.springframework.cloud.openfeign.EnableFeignClients;
-import org.springframework.context.annotation.ComponentScan;
+
 import org.springframework.context.annotation.Import;
 import org.springframework.scheduling.annotation.EnableScheduling;
-import org.springframework.security.oauth2.config.annotation.web.configuration.EnableResourceServer;
 import yun520.xyz.util.SpringContentUtils;
 
-import java.util.Arrays;
-import java.util.HashMap;
-import java.util.logging.Logger;
-import java.util.stream.IntStream;
 
 @SpringBootApplication
 @EnableFeignClients
