a17dce50a268deb2346ce9ea8862e29c04edc16b
1510557673@qq.com <Aa1997925>
2023-06-21 14:15:43 +0800

    修改流水线文件

--- 部署/start_jenkins.sh
+++ 部署/start_jenkins.sh
@@ -42,19 +42,19 @@ for ((i = 0; i < ${#jar_name[@]}; i++)); do
 
 	# MaxMetaspaceSize元空间  Xmx堆内存
 	if [ "${APP_NAME}" == "ydbspace_image.jar" ]; then
-	   echo "启动jar包：  java -jar -Xms128M -Xmx512M -XX:MetaspaceSize=128M -XX:MaxMetaspaceSize=512M  ${DEPLOY_PATH}/${APP_NAME} --spring.profiles.active=prod > ${APP_NAME}.log & "
+	   echo "启动jar包：  java -jar -Xms128M -Xmx512M -XX:MetaspaceSize=128M -XX:MaxMetaspaceSize=512M  ${DEPLOY_PATH}/${APP_NAME} --spring.profiles.active=prod > ./logs/${APP_NAME}.log & "
 
-		nohup java -jar -Xms128M -Xmx512M -XX:MetaspaceSize=128M -XX:MaxMetaspaceSize=512M "${DEPLOY_PATH}/${APP_NAME}" --spring.profiles.active=unraid > "${APP_NAME}.log" &
+		nohup java -jar -Xms128M -Xmx512M -XX:MetaspaceSize=128M -XX:MaxMetaspaceSize=512M "${DEPLOY_PATH}/${APP_NAME}" --spring.profiles.active=unraid > "./logs/${APP_NAME}.log" &
 
 	elif [ "${APP_NAME}" == "eurekaservice.jar" ]; then
-	  echo "启动jar包：  java -jar -Xms64M -Xmx256M -XX:MetaspaceSize=64M -XX:MaxMetaspaceSize=256M  ${DEPLOY_PATH}/${APP_NAME} --spring.profiles.active=prod > ${APP_NAME}.log & "
+	  echo "启动jar包：  java -jar -Xms64M -Xmx256M -XX:MetaspaceSize=64M -XX:MaxMetaspaceSize=256M  ${DEPLOY_PATH}/${APP_NAME} --spring.profiles.active=prod > ./logs/${APP_NAME}.log & "
 
-		nohup java -jar -Xms64M -Xmx256M -XX:MetaspaceSize=64M -XX:MaxMetaspaceSize=256M "${DEPLOY_PATH}/${APP_NAME}" --spring.profiles.active=unraid > "${APP_NAME}.log" &
+		nohup java -jar -Xms64M -Xmx256M -XX:MetaspaceSize=64M -XX:MaxMetaspaceSize=256M "${DEPLOY_PATH}/${APP_NAME}" --spring.profiles.active=unraid > "./logs/${APP_NAME}.log" &
 	else
-	  echo "启动jar包：  java -jar -Xms64M -Xmx128M -XX:MetaspaceSize=64M -XX:MaxMetaspaceSize=128M  ${DEPLOY_PATH}/${APP_NAME} --spring.profiles.active=prod > ${APP_NAME}.log & "
+	  echo "启动jar包：  java -jar -Xms64M -Xmx128M -XX:MetaspaceSize=64M -XX:MaxMetaspaceSize=128M  ${DEPLOY_PATH}/${APP_NAME} --spring.profiles.active=prod > ./logs/${APP_NAME}.log & "
 
 		# 指定生产环境包，必须用绝对路径
-		nohup java -jar -Xms64M -Xmx128M -XX:MetaspaceSize=64M -XX:MaxMetaspaceSize=128M "${DEPLOY_PATH}/${APP_NAME}" --spring.profiles.active=prod > "${APP_NAME}.log" &
+		nohup java -jar -Xms64M -Xmx128M -XX:MetaspaceSize=64M -XX:MaxMetaspaceSize=128M "${DEPLOY_PATH}/${APP_NAME}" --spring.profiles.active=prod > "./logs/${APP_NAME}.log" &
 	fi
 	sleep 3
 	# 等待程序执行
